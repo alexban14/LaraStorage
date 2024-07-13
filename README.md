71b1c5e166f0546d76238b45aeb33d506e1dcf63
Alex Ban <alexbanut10@gmail.com>
2024-07-13 22:59:04 +0300

    initial project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Google Drive demo for storing and sharing files
---
+++ src/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    server: {
+        host: true,
+        port: 6225,
+        hmr: {
+            host: 'localhost'
+        }
+    },
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+});
